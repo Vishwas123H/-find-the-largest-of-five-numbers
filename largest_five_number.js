d6dccf85c4c06f62f843cdfd56a494b91002c67f
Vishwas123H <112620299+Vishwas123H@users.noreply.github.com>
2022-11-11 18:55:11 +0530

    Add files via upload

---
+++ largest_five_number.js
@@ -0,0 +1,25 @@
+a=9;
+b=6;
+c=4;
+d=2;
+f=10;
+if (a>b && a>c && a>d && a>f)
+{
+    console.log(a);
+}
+else if (b>a && b>c && b>d && b>f)
+{
+    console.log(b);
+}
+else if (c>a && c>b && c>d && c>f)
+{
+    console.log(c);
+}
+else if (d>a && d>c && d>b && d>f)
+{
+    console.log(d);
+}
+else
+{
+    console.log(f);
+}
